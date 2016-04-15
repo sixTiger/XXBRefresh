604b1fc2589598a6e420d8fabd992fde762a57f0
xiaobing <xiaobing3@staff.sina.com.cn>
2016-04-15 11:22:41 +0800

    XXBReFresh
    创建XXBReFreshDemo

---
+++ XXBRefreshDemo/XXBRefreshDemo/XXBRefreshDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXBRefreshDemo
+//
+//  Created by xiaobing on 16/4/15.
+//  Copyright © 2016年 xiaobing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XXBRefreshDemo/XXBRefreshDemo/XXBRefreshDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XXBRefreshDemo
+//
+//  Created by xiaobing on 16/4/15.
+//  Copyright © 2016年 xiaobing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
